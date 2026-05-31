46bb86972fbd6c334b20c0a1ded20abbe85ba814
alexisBrckr <alexis.brucker@outlook.fr>
2026-05-31 23:38:53 +0200

    Initial Commit

---
+++ Body X/Body_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_XApp.swift
+//  Body X
+//
+//  Created by Guyacadeau3 on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Body_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
